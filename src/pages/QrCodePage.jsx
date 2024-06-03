ea300bc4abe6d71abaa51e059860403ec7101da8
Albastomi <albustomi679@gmail.com>
2024-06-03 10:35:58 +0700

    test

--- src/pages/QrCodePage.jsx
+++ src/pages/QrCodePage.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 import { QRCodeSVG } from "qrcode.react";
+import { Link } from "react-router-dom";
 
 const QrCodePage = () => {
   return (
     <div className="flex flex-col gap-4 justify-center items-center h-screen">
+      <Link to={"/product"}>Product</Link>
       <h1 className="text-xl font-semibold">Scan My Barcode </h1>
       <QRCodeSVG value="https://reactjs.org/" size={254} />
     </div>
